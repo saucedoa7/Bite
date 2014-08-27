cac6eb4fb28c639dfa5b142538c90ff5531f9cf9
Albert Saucedo <saucedoa7@gmail.com>
2014-08-26 19:30:01 -0500

    Login/User able to update login info in profile VC 7:26p

---
+++ Bite/AddFriendsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendsTableViewCell.h
+//  Bite
+//
+//  Created by Albert Saucedo on 8/26/14.
+//  Copyright (c) 2014 Bite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendsTableViewCell : UITableViewCell
+
+@end
---
+++ Bite/AddFriendsTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  AddFriendsTableViewCell.m
+//  Bite
+//
+//  Created by Albert Saucedo on 8/26/14.
+//  Copyright (c) 2014 Bite. All rights reserved.
+//
+
+#import "AddFriendsTableViewCell.h"
+
+@implementation AddFriendsTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
