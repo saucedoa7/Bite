d68b9f8a6e34d0b3c46e47ac809b1503b801a566
Albert Saucedo <saucedoa7@gmail.com>
2014-08-27 09:19:42 -0500

    Day 3 PFquery for addFriendsVC

--- Bite/AddFriendsViewController.m
+++ Bite/AddFriendsViewController.m
@@ -10,29 +10,20 @@
 #import "AddFriendsTableViewCell.h"
 
 #define Username @"username"
+#define UserClass @"User"
 
-@interface AddFriendsViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface AddFriendsViewController ()<UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 @property NSMutableArray *friends;
-@interface AddFriendsViewController ()
 @property (strong, nonatomic) IBOutlet UISearchBar *searchFriends;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation AddFriendsViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -48,19 +39,32 @@
     return  self.friends.count;
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    AddFriendsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"addFriendsCellID"];
+    PFObject *friend = [self.friends  objectAtIndex:indexPath.row];
+
+    cell.textLabel.text = [friend objectForKey:Username];
+
+    NSLog(@"%@", cell.textLabel.text);
+}
 
 -(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
     self.friends = [NSMutableArray new];
 
-    PFQuery *restaurantQuery = [PFQuery queryWithClassName:@"Restaurant"];
-    [restaurantQuery whereKey:@"restaurantName" containsString:self.restaurantSearchField.text];
-    [restaurantQuery orderByAscending:@"restaurantName"];
-    [restaurantQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+    PFQuery *findFriendQuery = [PFUser query];
+    NSLog(@"TEst %@", self.searchFriends.text);
+
+    [findFriendQuery whereKey:Username containsString:self.searchFriends.text];
+    [findFriendQuery orderByAscending:Username];
+    [findFriendQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        NSLog(@"%@", objects);
         self.friends = [objects mutableCopy];
         [self.tableView reloadData];
     }];
-    [self.restaurantSearchField resignFirstResponder];
+
+
+    [self.searchFriends resignFirstResponder];
 
 }
 
